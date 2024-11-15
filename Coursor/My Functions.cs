963f066e31c7d51cfc3d799e098f0a8b667df222
ShabanHassan <shabanhassan1010@gmail.com>
2024-11-15 18:44:23 +0200

    Added Class to put on it all functions

---
+++ Coursor/My Functions.cs
@@ -0,0 +1,68 @@
+﻿
+namespace Coursor
+{
+    public static class My_Functions
+    {
+        public static void ExecuteMyOperations(int currentSelection)
+        {
+            Console.WriteLine($"{(currentSelection == 1 ? ">>> " : "    ")}1. Sum");
+            Console.WriteLine($"{(currentSelection == 2 ? ">>> " : "    ")}2. Multiply");
+            Console.WriteLine($"{(currentSelection == 3 ? ">>> " : "    ")}3. Divide");
+            Console.WriteLine($"{(currentSelection == 4 ? ">>> " : "    ")}4. Exit");
+        }
+        public static void PrintSum()
+        {
+            Console.WriteLine("You selected Sum.");
+            Console.Write("Enter Number One: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Number Two: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int res = num1 + num2;
+            Console.WriteLine($"Result: {res}");
+        }
+        public static void PrintMultiply()
+        {
+            Console.WriteLine("You selected Multiply.");
+            Console.Write("Enter two numbers: ");
+            Console.Write("Enter Number One: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Number Two: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int res = num1 * num2;
+            Console.WriteLine($"Result: {res}");
+        }
+        public static void PrintDivide()
+        {
+            Console.WriteLine("You selected Divide.");
+            Console.Write("Enter two numbers: ");
+            Console.Write("Enter Number One: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Number Two: ");
+            int num2 = int.Parse(Console.ReadLine());
+            if (num2 == 0)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
+            else
+            {
+                int res = num1 / num2;
+                Console.WriteLine($"Result: {res}");
+            }
+        }
+        public static bool ExitProgram()
+        {
+            Console.Write("Are you sure you want to exit? (y/n): ");
+            char confirmExit = Console.ReadKey(true).KeyChar;
+            if (confirmExit == 'y' || confirmExit == 'Y')
+            {
+                Console.WriteLine("\nExiting...");
+                return false; // Stop the program
+            }
+            else
+            {
+                Console.WriteLine("\nExit canceled. Returning to menu...");
+                return true; // Continue the program
+            }
+        }
+    }
+}
--- Coursor/Program.cs
+++ Coursor/Program.cs
@@ -11,7 +11,7 @@
             while (running)
             {
                 Console.Clear(); // Clear the screen every time
-                ExecuteMyOperations(position);
+                My_Functions.ExecuteMyOperations(position);
 
                 // Wait for user input
                 key = Console.ReadKey(true).Key;
@@ -21,16 +21,16 @@
                     switch (position)
                     {
                         case 1:
-                            PrintSum();
+                            My_Functions.PrintSum();
                             break;
                         case 2:
-                            PrintMultiply();
+                            My_Functions.PrintMultiply();
                             break;
                         case 3:
-                            PrintDivide();
+                            My_Functions.PrintDivide();
                             break;
                         case 4:
-                            running = ExitProgram();
+                            running = My_Functions.ExitProgram();
                             break;
                     }
                     if (running)
@@ -52,63 +52,6 @@
                         position = 1;
                 }
             }
-        }
-
-        static void ExecuteMyOperations(int currentSelection)
-        {
-            Console.WriteLine($"{(currentSelection == 1 ? ">>> " : "    ")}1. Sum");
-            Console.WriteLine($"{(currentSelection == 2 ? ">>> " : "    ")}2. Multiply");
-            Console.WriteLine($"{(currentSelection == 3 ? ">>> " : "    ")}3. Divide");
-            Console.WriteLine($"{(currentSelection == 4 ? ">>> " : "    ")}4. Exit");
-        }
-        static void PrintSum()
-        {
-            Console.WriteLine("You selected Sum.");
-            Console.Write("Enter two numbers: ");
-            int num1 = int.Parse(Console.ReadLine());
-            int num2 = int.Parse(Console.ReadLine());
-            int res = num1 + num2;
-            Console.WriteLine($"Result: {res}");
-        }
-        static void PrintMultiply()
-        {
-            Console.WriteLine("You selected Multiply.");
-            Console.Write("Enter two numbers: ");
-            int num1 = int.Parse(Console.ReadLine());
-            int num2 = int.Parse(Console.ReadLine());
-            int res = num1 * num2;
-            Console.WriteLine($"Result: {res}");
-        }
-        static void PrintDivide()
-        {
-            Console.WriteLine("You selected Divide.");
-            Console.Write("Enter two numbers: ");
-            int num1 = int.Parse(Console.ReadLine());
-            int num2 = int.Parse(Console.ReadLine());
-            if (num2 == 0)
-            {
-                Console.WriteLine("Cannot divide by zero.");
-            }
-            else
-            {
-                int res = num1 / num2;
-                Console.WriteLine($"Result: {res}");
-            }
-        }
-        static bool ExitProgram()
-        {
-            Console.Write("Are you sure you want to exit? (y/n): ");
-            char confirmExit = Console.ReadKey(true).KeyChar;
-            if (confirmExit == 'y' || confirmExit == 'Y')
-            {
-                Console.WriteLine("\nExiting...");
-                return false; // Stop the program
-            }
-            else
-            {
-                Console.WriteLine("\nExit canceled. Returning to menu...");
-                return true; // Continue the program
-            }
-        }
+        }       
     }
 }
